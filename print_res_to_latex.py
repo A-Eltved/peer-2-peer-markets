9201cd622f3d6e7624a18fca46b3fe716d4879b4
anderseltved@gmail.com <aelt@dtu.dk>
2018-08-15 10:46:23 +0200

    Changed table plotting format. Added part 4 to main notebook.

--- print_res_to_latex.py
+++ print_res_to_latex.py
@@ -1,4 +1,4 @@
-def write_schedule(p,filename='out_schedule.txt',caption='Schedule ...'):
+def write_schedule(p,filename='out_schedule.txt',caption='Schedule ...',label = ''):
     print('Writing table...')
     with open(filename,'w') as f:
         # Write header
@@ -6,20 +6,28 @@ def write_schedule(p,filename='out_schedule.txt',caption='Schedule ...'):
         f.write('\\caption{'+caption+'}\n')
         f.write('\\centering\n')
         f.write('\\begin{tabular}{l|cccccccccccccc}\n')
-        f.write('Agent &  1  &   2   &   3   &   4  &  5  &  6  &  7   &   8  &  9  &  10 &  11 &  12 &  13  & 14  \\\\ \\hline\n')
-        f.write('$p_i$ [MW] &')
-        for i in range(len(p.value)):
+        f.write('Agent &  1  &   2   &   3   &   4  &  5  &  6  &  7   \\\\ \\hline\n')
+        f.write('$p^\star_i$ [MW] &')
+        for i in range(7):
+            f.write('{:.2f}'.format(p.value.item(i)))
+            if not i == 6:
+                f.write(' & ')
+            else: 
+                f.write(' \\\\ \\hline\\hline\n')
+        f.write('Agent &   8  &  9  &  10 &  11 &  12 &  13  & 14  \\\\ \\hline\n')
+        f.write('$p^\star_i$ [MW] &')
+        for i in range(7,len(p.value)):
             f.write('{:.2f}'.format(p.value.item(i)))
             if not i == len(p.value)-1:
                 f.write(' & ')
             else: 
                 f.write(' \n')
         f.write('\\end{tabular}\n')
-        f.write('\\label{tab:}\n')
+        f.write('\\label{tab:'+label+'}\n')
         f.write('\\end{table}\n')
     print('Done.')
     
-def write_pricing(p,price,filename='out_price.txt',caption='Payments and revenues ...'):
+def write_pricing(p,price,filename='out_price.txt',caption='Payments and revenues ...',label = ''):
     print('Writing pricing table...')
     with open(filename,'w') as f:
         # Write header
@@ -27,15 +35,23 @@ def write_pricing(p,price,filename='out_price.txt',caption='Payments and revenue
         f.write('\\caption{'+caption+'}\n')
         f.write('\\centering\n')
         f.write('\\begin{tabular}{l|cccccccccccccc}\n')
-        f.write('Agent &  1  &   2   &   3   &   4  &  5  &  6  &  7   &   8  &  9  &  10 &  11 &  12 &  13  & 14  \\\\ \\hline\n')
-        f.write('Price [\\$] &')
-        for i in range(len(p.value)):
+        f.write('Agent &  1  &   2   &   3   &   4  &  5  &  6  &  7  \\\\ \\hline\n')
+        f.write('Payment/revenue [\\$] &')
+        for i in range(7):
+            f.write('{:.2f}'.format(p.value.item(i)*price))
+            if not i == 6:
+                f.write(' & ')
+            else: 
+                f.write(' \\\\ \\hline \\hline \n')
+        f.write('Agent &  8  &  9  &  10 &  11 &  12 &  13  & 14  \\\\ \\hline\n')
+        f.write('Payment/revenue [\\$] &')
+        for i in range(7,len(p.value)):
             f.write('{:.2f}'.format(p.value.item(i)*price))
             if not i == len(p.value)-1:
                 f.write(' & ')
             else: 
                 f.write(' \n')
         f.write('\\end{tabular}\n')
-        f.write('\\label{tab:}\n')
+        f.write('\\label{tab:'+label+'}\n')
         f.write('\\end{table}\n')
     print('Done.')
